6ca410b39d31dc0abb7a12b7b2ed739e92efc565
Isaias-fernandes <isaiasf59@gmail.com>
2026-09-26 17:24:51 -0300

    Refresh app cache for medication removal fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gestao-farma-v22';
+const CACHE='gestao-farma-v23';
 const SHELL=['./','./index.html','./app.js','./resumo-painel.js','./melhorias-v12.js','./correcao-estoque.js','./pedidos-v2.js','./correcao-validade.js','./exclusao-permanente.js','./exclusao-paciente.js','./xlsx-download-fix.js','./config.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
